849e0cae875f40418f3efdfe9053b056a097a1fb
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-09-08 16:38:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function get(){
+	const url=`https://openweathermap.org/api`;
+	fetch(url)
+	.then((response)=>response.json())
+	.then(data=>{
+		document.getElementById("para").style.display=block;
+		document.getElementById("weatherData").innerText=data;
+	})
+}
+const button=document.getElementById("btn");
+button.addEventListener("click",get);
